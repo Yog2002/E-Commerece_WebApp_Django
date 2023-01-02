ab0979f7452ae3fa5ddd47487e7d09ce42d651db
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2023-01-02 22:56:35 +0530

    Initial Commit

---
+++ Blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse #Created Manually
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("We Are At AboutUs")
+#render(request,"Shop/index.html")
+
+def about(request):
+    return HttpResponse("We Are At AboutUs")
+
+def contact(request):
+    return HttpResponse("We Are At ContactUs")
+
+def tracker(request):
+    return HttpResponse("We Are At Tracker")
+
+def search(request):
+    return HttpResponse("We Are At Search")
+
+def proudView(request):
+   return HttpResponse("We Are At ProductView")
+
+def checkout(request):
+    return HttpResponse("We Are At Checkout")
+
